f9e1114e9b41ce6a78acc5c0b455725762b67517
Barshon Basak <127104156+barshon-basak@users.noreply.github.com>
2023-08-05 20:33:53 +0600

    Create Lab_task1.java

---
+++ Lab_task1.java
@@ -0,0 +1,23 @@
+//TASK 1 : Lab
+
+package javaapplication3;
+
+import java.util.Scanner;
+
+public class JavaApplication3 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        String name;
+        int age;
+        String dept;
+        
+        name = "Barshon Basak";
+        age =21;
+        dept = "CSE";
+        
+        System.out.print("Name is " +name+ " Age is " +age+ " Department is "+dept);
+    }
+    
+}
