d0e494cc6a11f139081eca3b4010549501ed546e
Barshon Basak <127104156+barshon-basak@users.noreply.github.com>
2023-10-31 13:11:45 +0600

    Create NormalClass.java

---
+++ GenericClass/NormalClass.java
@@ -0,0 +1,18 @@
+
+package javaapplication43;
+
+public class NOrmalClass {
+
+  private double id;
+
+    public NOrmalClass(double id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "NormalClass : " + "id= " + id;
+    }
+        
+        
+    }
