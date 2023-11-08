07a5b87751b4170cc2b0f43056eb168c0ab3eaed
Barshon Basak <127104156+barshon-basak@users.noreply.github.com>
2023-11-09 01:13:44 +0600

    Create ShapeClass.java

---
+++ ShapeObject Using OOP(Basic)/ShapeClass.java
@@ -0,0 +1,26 @@
+
+package javaapplication54;
+
+public class Shape {
+    private String name;
+
+    public Shape(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Shape{" + "name=" + name + '}';
+    }
+
+    
+    
+}
