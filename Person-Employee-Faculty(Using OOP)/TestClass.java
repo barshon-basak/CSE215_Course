bb66895215a6baf06296f118104c4b623b729063
Barshon Basak <127104156+barshon-basak@users.noreply.github.com>
2023-11-09 01:33:27 +0600

    Create TestClass.java

---
+++ Person-Employee-Faculty(Using OOP)/TestClass.java
@@ -0,0 +1,11 @@
+
+package javaapplication55;
+
+public class JavaApplication55 {
+
+    public static void main(String[] args) {
+       Faculty faculty = new Faculty("John Doe", "Male", 40, "EMP123", "Computer Science", 60000, "JD", "Professor");
+        System.out.println(faculty.toString());
+    }
+    
+}
