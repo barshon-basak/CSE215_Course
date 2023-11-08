b7e2625fbf16a374ff81e7e3d959467d0715c14e
Barshon Basak <127104156+barshon-basak@users.noreply.github.com>
2023-11-09 01:51:39 +0600

    Create PersonClass.java

---
+++ Person-Employee-PartTime-FullTime (OOP)/PersonClass.java
@@ -0,0 +1,27 @@
+
+package javaapplication56;
+
+
+public class Person {
+    private String name;
+    private int age;
+    private String address;
+
+    public Person(String name, int age, String address) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+}
