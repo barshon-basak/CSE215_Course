74d3650149797ec65cf64d463f9f74baa73aba46
Barshon Basak <127104156+barshon-basak@users.noreply.github.com>
2023-11-09 01:34:08 +0600

    Create EmployeeClass.java

---
+++ Person-Employee-Faculty(Using OOP)/EmployeeClass.java
@@ -0,0 +1,42 @@
+
+package javaapplication55;
+
+
+public class Employee extends Person{
+    private String id;
+    private String department;
+    private double salary;
+
+    public Employee(String name, String gender, int age, String id, String department, double salary) {
+        super(name, gender, age);
+        this.id = id;
+        this.department = department;
+        this.salary = salary;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+    
+    
+}
