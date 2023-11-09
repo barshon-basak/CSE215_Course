7f1e2d07bf457b6cec3801de3cf767f806e666a3
Barshon Basak <127104156+barshon-basak@users.noreply.github.com>
2023-11-09 20:34:09 +0600

    Create QuizClass.java

---
+++ FILE IO/Problem_01/QuizClass.java
@@ -0,0 +1,14 @@
+
+package javaapplication62;
+
+public class Quiz {
+    public String id;
+    public int marks;
+
+    public Quiz(String id, int marks) {
+        this.id = id;
+        this.marks = marks;
+    }
+    
+    
+}
